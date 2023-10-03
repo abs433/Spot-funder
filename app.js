19f06a2851be080a8f57c647edcd6fe24bcb6975
abubakarabs433@gmail.com <abubakarabs433@gmail.com>
2023-10-03 14:01:57 +0500

    alert

---
+++ app.js
@@ -0,0 +1,10 @@
+// var myName = :"My Name is Abu Bakar" ;
+var myName = "my name is Abu Bakar"
+
+
+
+alert("Please enter a valid password");
+alert("Wellcome to JS land... ");
+ alert("Happy Codding");
+alert("Hello... I can run JS through my web browser's console");
+alert("My name is abu bakar");
