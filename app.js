25821d3ba7b9118cdb39eaebb556fe359cb74534
abubakarabs433@gmail.com <abubakarabs433@gmail.com>
2023-11-20 14:41:20 +0500

    java3

---
+++ app.js
@@ -0,0 +1,30 @@
+var age = "I am 25 years old";
+alert(age);
+
+var visit = "You have visited this site 14 times";
+alert(visit);
+
+
+
+// document.write("My birth years ");
+// var birthYear = prompt("my birth years ");
+// document.write(prompt("birthYear"));
+
+// document.write(" Data type of my ddeclared variable is number ");
+ 
+
+
+var visitorName = prompt(" enter your name ");
+
+var products = prompt(" enter products ");
+
+var quantity = prompt(" enter products quantity ");
+
+var order = ("Your Name : " + visitorName  + " Your Item : " + products  + " Quantity : " + quantity );
+
+document.write(order)
+
+// alert("visitorName  ");
+// prompt("inter Your name")
+// document.write("Your name");
+
